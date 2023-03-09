42a0ab4f080c6c85863e4ee8df38f3b3a0e1b0a3
manasan-iTL <daiyaofssh2020@gmail.com>
2023-03-09 21:16:31 +0900

    add ハンバーガーメニューの実装 ・メニュー開閉時のState、更新関数の追加 ・ハンバーガーメニューのスタイル実装

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,20 @@ import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Stack from '@mui/material/Stack'
 
+import Link from "next/link"
+import classes from "@/styles/header.module.css"
+import { useCallback, useState } from "react"
 
 const Header = () => {
+
+    const [hamburgerIsOpen, setHamburgerIsOpen] = useState(false)
+
+    const clickHambergerHandler = useCallback(() => {
+        setHamburgerIsOpen((prevSate) => !prevSate)
+    }, [])
+
+    console.log(hamburgerIsOpen)
+
     return (
         <header className={classes.header}>
             <h1 className={`${classes.heading}`}>
@@ -17,6 +29,11 @@ const Header = () => {
                         C-questionare
                 </Link>
             </h1>
+            <div className={`${classes.hamburger_btn} ${hamburgerIsOpen? classes.active: ""}`} onClick={clickHambergerHandler}>
+                <span className={classes.hamburger_line}></span>
+                <span className={classes.hamburger_line}></span>
+                <span className={classes.hamburger_line}></span>
+            </div>
         </header>
     )
 }
