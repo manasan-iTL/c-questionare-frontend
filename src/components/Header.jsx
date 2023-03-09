a012a1c81e29e9836cf23401f90df5a45d8e3ccf
manasan-iTL <daiyaofssh2020@gmail.com>
2023-03-09 21:17:29 +0900

    delete 不要な部分の削除

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,3 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Stack from '@mui/material/Stack'
 
 import Link from "next/link"
 import classes from "@/styles/header.module.css"
@@ -26,7 +17,7 @@ const Header = () => {
         <header className={classes.header}>
             <h1 className={`${classes.heading}`}>
                 <Link href="#" className={classes.link}>
-                        C-questionare
+                  C-questionare
                 </Link>
             </h1>
             <div className={`${classes.hamburger_btn} ${hamburgerIsOpen? classes.active: ""}`} onClick={clickHambergerHandler}>
