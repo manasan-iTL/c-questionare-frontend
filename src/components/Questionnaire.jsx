0d78a763619cc2e4446b02fea1bce02474e7388c
manasan-iTL <daiyaofssh2020@gmail.com>
2023-03-17 14:05:36 +0900

    change 固定値をPropsで動的に変更できるように ・Propsへの置き換え ・timeの分割処理

--- src/components/Questionnaire.jsx
+++ src/components/Questionnaire.jsx
@@ -1,15 +1,22 @@
 import Link from "next/link"
 import classes from "@/styles/questionnaire.module.css"
+import { useCallback, useMemo } from "react"
 
 const Questionnaire = (props) => {
+
+    const time = useMemo(() => {
+        const time = props.closed_at.split("-")
+        return time
+    }, [props.closed_at])
+
     return (
         <div>
             <Link 
                 href="#"
                 className={`${classes.question_box} ${props.isPriority? classes.question_primary_color: classes.question_normal_color}`}>
-                <span className={classes.question_medium_text}>(2022年O月OO日まで)</span>
-                <span className={classes.question_medium_text}>～～～に関するアンケート</span>
-                <span className={classes.question_small_text}>富島悠介</span>
+                <span className={classes.question_medium_text}>({time[0]}年{time[1]}月{time[2]}日まで)</span>
+                <span className={classes.question_medium_text}>{props.title}</span>
+                <span className={classes.question_small_text}>{props.author}</span>
             </Link>
         </div>
     )
