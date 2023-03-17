0f8f73143d2f7a36735f95ec5771e41fb2a7af06
manasan-iTL <daiyaofssh2020@gmail.com>
2023-03-17 14:06:35 +0900

    change Propsを変更 ・valueを追加

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -2,13 +2,16 @@
 import classes from "@/styles/inputField.module.css"
 
 const InputField = (props) => {
+
+    console.log(props.value)
+
     return (
         <div className={classes.container}>
             <label htmlFor={props.name} className={classes.input_label}>{props.label}</label>
             <input 
                 type={props.type} 
                 onChange={props.onChange}
-                ref={props.element}
+                value={props.value}
                 id={props.name}
                 name={props.name}
                 className={classes.input_item}
